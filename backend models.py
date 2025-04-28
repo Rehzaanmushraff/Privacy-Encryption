d36f87f8fa4def6ec216bf15e6b1de13ab1efd7b
Rehzaanmushraff <mushraffrehz@gmail.com>
2025-04-28 14:24:55 +0530

    Delete backend models.py

--- backend models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class Message(Base):
-    __tablename__ = "messages"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    original_message = Column(String)
-    encrypted_message = Column(String)
